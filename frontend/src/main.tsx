f5c2173b4cfb6cd17239660e14bbb7e1baf54474
Ednan Rogério Frizzera Filho <ednan@frizzera.dev>
2025-08-22 14:13:41 -0300

    feat(pages): add HomePage and Layout components with routing setup
    - Introduced `HomePage` component as the main landing page.
    - Created `Layout` component to structure the application with header and footer.
    - Implemented React Router for navigation, replacing the previous `App` component.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+// Pages
+import HomePage from './pages/HomePage.tsx';
+
+import './index.css';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />,
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
